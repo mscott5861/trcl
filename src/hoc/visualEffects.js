4f1dfff8e69768640a3acecb994de849c576a0ed
Matt Adams <matt@forestgiant.com>
2019-02-08 14:24:18 -0500

    Added landscape orientation handling to withParallax HOC; also checked to ensure stylesheet not extant before appending.

--- src/hoc/visualEffects.js
+++ src/hoc/visualEffects.js
@@ -12,7 +12,8 @@ const ParallaxGroup = styled.div`
     right: 0;
     bottom: 0;
     left: 0;
-    overflow-y: scroll;
+    overflow-y: ${props => props.pageOrientation === 'portrait' ? 'scroll' : 'initial'};
+    overflow-x: ${props => props.pageOrientation === 'portrait' ? 'initial' : 'scroll'};
     transform: ${props => props.depth ? 'translateZ(' + props.depth + 'px) scale(' + (1 + (props.depth * -1)) + ') !important' :
                 'translateZ(-0.5px) scale(1.5) !important'};
     z-index: -10;
@@ -20,24 +21,27 @@ const ParallaxGroup = styled.div`
 }`
 
 
-export const withParallax = (WrappedComponent, depth) => {
+export const withParallax = (WrappedComponent, depth, pageOrientation = 'portrait') => {
   return class extends React.Component {
     constructor() {
       super();
       let style = document.createElement('style');
       style.type = 'text/css';
+      style.id = 'withParallaxHOC';
       style.innerHTML = `.plx { 
         perspective: 1px;
         height: 100vh;
-        overflow-x: hidden;
-        overflow-y: scroll;
-      }
+        overflow-x: ` + (pageOrientation === 'portrait' ? 'hidden;' : 'scroll;') +
+       `overflow-y: ` + (pageOrientation === 'portrait' ? 'scroll;' : 'hidden;') +
+       `}
       html {
         overflow: hidden;
-      } 
-      `;
- 
-      document.getElementsByTagName('head')[0].appendChild(style);
+      }`;
+      
+      let styleSheet = document.getElementById('withParallaxHOC');
+      if (styleSheet === null) {
+        document.getElementsByTagName('head')[0].appendChild(style);
+      }
     }
 
     componentWillMount() {
@@ -51,7 +55,8 @@ export const withParallax = (WrappedComponent, depth) => {
     render() {
       return(
         <ParallaxGroup
-          depth={-1 * depth}>
+          depth={-1 * depth}
+          pageOrintation={pageOrientation}>
           <WrappedComponent
             {...this.props} />
         </ParallaxGroup>
--- src/views/ViewParallax.js
+++ src/views/ViewParallax.js
@@ -4,11 +4,16 @@ import { Copy, Image, PaddedContent } from 'components'
 import { withParallax } from 'hoc'
 import { generateLoremIpsum } from 'utilities'
 
-const ParallaxedImage = withParallax(Image, 4);
+const ParallaxedImage = withParallax(Image, 6);
 const StContainer = styled.div`
   position: absolute;
   padding: 2rem;
   z-index: 10;
+`
+
+const StLandscapeContainer = styled.div`
+  width: 400vw;
+  padding: 3rem;
 `
 
 export default class ViewParallax extends React.Component {
@@ -22,7 +27,6 @@ export default class ViewParallax extends React.Component {
             { generateLoremIpsum() }
           </Copy>
         </StContainer>
-
         <ParallaxedImage
           src={'./img/sample-img-04.jpg'}/>
       </PaddedContent>
